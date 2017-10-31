94ba8ccc5d9241b6944147edb02bfe69113a97b9
Dušan Živković <dusan.zivkovic@cadev.rs>
2017-10-31 12:05:08 +0100

    pokusavam put

--- MovieTicketsBackend/router.js
+++ MovieTicketsBackend/router.js
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 // Import index action from movies controller
 import { index } from './controllers/movies';
+import { putty } from './controllers/putmovies';
 
 // Initialize the router
 const router = Router();
@@ -8,5 +9,6 @@ const router = Router();
 // Handle /movies.json route with index action from movies controller
 router.route('/movies.json')
   .get(index);
-
+//router.route('/put')
+//  .get(putty);
 export default router;
